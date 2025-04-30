379a3471528c976d68083c2efafa9d3fcfb796fd
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-30 02:05:05 -0400

    Created loading and tours state constants

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+import Loading from './components/Loading';
+import Tours from './components/Tours';
 
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
   return (
     <div>
