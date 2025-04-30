02240834b0dd8ea0ec84e1dfe211dc453f9a381b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-30 03:01:15 -0400

    displayed tour info and not interested button

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,8 +1,16 @@
-function Tour() {
+function Tour({id, image, info, price, name}) {
     return (
-        <div>
-            Tour
-        </div>
+        <article className="single-tour">
+            <img src={image} alt={name} />
+            <footer>
+                <div className="tour-info">
+                    <h4>{name}</h4>
+                    <h4 className="tour-price">${price}</h4>
+                </div>
+                <p>{info}</p>
+                <button className="delete-btn">Not Interested</button>
+            </footer>
+        </article>
     )
 }
 
