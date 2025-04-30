87ba969a5d929997e572e2287484e545ec365f1b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-04-30 02:57:15 -0400

    Created Tour component and mapped tours

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { useState } from 'react';
 import Tours from './components/Tours';
 import data from './Tours';
 
-
 function App() {
   const [tours, setTours] = useState(data);
+  console.log(tours);
 
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} />
     </main>
   )
 
--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,8 +1,18 @@
-function Tours() {
+import Tour from './Tour';
+
+function Tours({tours}) {
     return (
-        <div>
-            Tours
-        </div>
+        <section>
+            <div className="title">
+                <h2>Our Tours</h2>
+                <div className="underline"></div>
+            </div>
+            <div>
+                {tours.map((tour) => {
+                    return <Tour key={tour.id} ></Tour>
+                })}
+            </div>
+        </section>
     )
 }
 
